154795009440519b1a83a07947994002659303ee
Caleb Whiting <diamondonahill@gmail.com>
2016-10-22 13:58:45 -0400

    working prototype

---
+++ public/main.js
@@ -0,0 +1,39 @@
+const names = {
+  1: 'Computer',
+  2: 'Camera 1',
+  3: 'Camera 2',
+  4: 'Camera 3',
+  5: 'Camera 4',
+  6: 'Leo-Marc',
+  7: 'Camera 6',
+  8: 'Camera 7',
+  9: 'Camera 8',
+  10: 'Camera 9',
+}
+
+var id = Number(prompt('enter your camera number')) + 1
+
+var socket = io()
+
+socket.on('update', function(state) {
+
+  // log to console
+  console.log(state, id)
+
+  // update indicator bar
+  if (state.program == id)
+    document.body.classList.add('program')
+  else if (state.preview == id)
+    document.body.classList.add('preview')
+
+  if (state.program != id)
+    document.body.classList.remove('program')
+  else if (state.preview != id)
+    document.body.classList.remove('preview')
+
+  // update status text
+  const programName = names[state.program]
+  const previewName = names[state.preview]
+  document.querySelector('.status').innerHTML = `Program: ${programName}<br/>Preview: ${previewName}`
+
+})
--- server.js
+++ server.js
@@ -6,14 +6,10 @@ const Atem = require('atem')
 const path = require('path')
 var io = require('socket.io')(http);
 
-const atem = new Atem('192.16.10.240')
+const atem = new Atem('192.168.10.240')
 
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
-
-io.on('connection', (socket) => {
-  console.log('a client connected')
-})
 
 const tallyState = {
   program: 0,
@@ -25,6 +21,11 @@ const emitUpdate = () => {
   io.emit('update', tallyState)
 }
 
+io.on('connection', (socket) => {
+  console.log('a client connected')
+  emitUpdate()
+})
+
 atem.on('previewBus', id => {
   tallyState.preview = id
   emitUpdate()
@@ -34,8 +35,30 @@ atem.on('programBus', id => {
   emitUpdate()
 })
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-})
+http.listen(80, function(){
 
-setInterval(() => emitUpdate(), 500)
+  console.log('listening on *:3000');
+
+  var os = require('os');
+  var ifaces = os.networkInterfaces();
+
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        console.log(ifname + ':' + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        console.log(ifname, iface.address);
+      }
+      ++alias;
+    });
+  });
+})
